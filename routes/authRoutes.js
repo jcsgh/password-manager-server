02236284f81f73e6b4067aa52cfdbd673e1c5974
jcsgh <business.joshuac@gmail.com>
2022-03-15 14:38:43 -0500

    register route 2

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,44 @@
 const express = require('express')
+const bcrypt = require('bcrypt')
+// const jwt = require('jsonwebtoken')
+// const verifyJWT = require('../middleware/jwt')
+
+
 const register = require('../schemas/registerSchema')
-const login = require('../schemas/loginSchema')
+// const login = require('../schemas/loginSchema')
 
 // Create the Router
 const authRouter = express.Router()
 
 authRouter.get("/", (req, res) => {
     res.status(200).json({message: "Auth routes up"})
+})
+
+authRouter.post("/register", async(req, res)=>{
+    let user = req.body
+    let password = user.password
+    let salt = Number(process.env.SALT)
+    user.created_at = Date.now()
+    user.completed = false
+
+    //TODO Make DataValidation Middleware LATER
+    if (!user.firstName || !user.lastName || !user.userName || !user.email || !user.password){
+        res.status(400).json({message: "Please enter a firstName, lastName, userName, email, and password"})
+    }
+
+    // TODO Make helper
+    let hashedPassword = await bcrypt.hash(password, salt)
+    user.password = hashedPassword
+    
+    register.create(user, (error, result)=>{
+        if(error){
+            res.status(400).json({message: error.message})
+        }
+        if(result ===  null || result === []){
+            res.status(400).json({message: error.message})
+        }
+        res.status(201).json({user: result})
+    })
 })
 
 module.exports = authRouter
--- routes/passwordRoutes.js
+++ routes/passwordRoutes.js
@@ -18,31 +18,5 @@ passwordRouter.get("/users", (req, res)=>{
     })
 })
 
-passwordRouter.post("/register", (req, res)=>{
-    const user = req.body
-    user.created_at = Date.now()
-    user.completed = false
-    register.create(user, (error, result)=>{
-        if(error){
-            res.status(400).json({message: error.message})
-        }
-        if(result ===  null || result === []){
-            res.status(400).json({message: error.message})
-        }
-        res.status(201).json({user: result})
-    })
-})
-
-passwordRouter.get("/users", (req, res)=>{
-    login.find((error, result)=>{
-        if(error){
-            res.status(500).json({message: error.message})
-        }
-        if(result ===  null || result === []){
-            res.status(404).json({message: error.message})
-        }
-        res.status(200).json({message: "YEAH"})
-    })
-})
 
 module.exports = passwordRouter
